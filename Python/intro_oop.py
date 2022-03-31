e58fa44889e0b3b4219cd5e90e7628af1f1c4a02
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-03-31 14:04:13 +0100

    updates

---
+++ Python/intro_oop.py
@@ -0,0 +1,3 @@
+"""Introduction to Object oriented Programming(OOP) OOP is one of the most vital parts in python. this topic is quite confusing for a lot of beginners but i will
+ try to make it as simple as possible. So we have different examples of objects in python e.g 
+ integers,strings,e.t.c i will explain with an example """
