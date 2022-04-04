829188dd16e217b0a6c59448b7bbd0692aeeb137
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-04-04 14:25:44 +0100

    added compliment game

--- Python/compliment_game.py
+++ Python/compliment_game.py
@@ -1,22 +1,26 @@
+from ast import Continue
 import sys,random
 
 print('Hello! User, !')
 print('Welcome to The Comment game')
 print('Lets See the comments you got')
-comment=('Have a great ','Brush your ', 'Be mindful of ', 'Take care of your ', 'Good ', 'Hug a ', 'Kiss your ','Eat your y')
-ending =('Burger','Teeth','Snake','Day','Mind','Flowers','Week','Frog','yourself')
-
-while True:
+comment=('Have a great ','Brush your ', 'Be mindful of ', 'Take care of your ', 'Good ', 'Hug a ', 'Kiss your ','Eat your ')
+ending =('Burger','Teeth','Snakes','Day','Mind','Flowers','Week','Frog','yourself')
+cont = True
+while cont:
     firstcomment=random.choice(comment)
     endingcomment = random.choice(ending)
     
     print('*' * 40)
-    print('{},{}'.format(firstcomment,endingcomment))
+    print('{}{}'.format(firstcomment,endingcomment))
     print('*' * 40)
 
-    try_again=input('\n To Try again (Press Y )\n\n To Exit(Press Q)')
+    try_again=input('\n To Try again (Press Y )\tTo Exit(Press Q)\n')
     match try_again.lower():
-         case 'n':
-             break
+         case 'q':
+             print('\nExiting game........')
+             cont = False
          case 'y':
-               input("\nPress Enter to exit.")
+               continue
+            
+        
