26da6c418c82b5f491bd398670e461a72f872c03
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-05-04 23:57:40 +0100

    added match case function

--- Python/multiply_table.py
+++ Python/multiply_table.py
@@ -3,9 +3,16 @@
 This is a multiplication table which allows the user to input a number 
 and the algorithm outputs a multiplcation table
 """
+from distutils.log import error
+
+
 num = int(input('Enter a number: '))
 int_num = 0
 while int_num <=12:
     ans = num* int_num
     print(' {}   +  {} =  {} '.format(num,int_num,ans))
     int_num = int_num + 1
+
+    match error:
+        case ValueError:
+            print('Error must try again ')
