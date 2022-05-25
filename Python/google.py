c458364ca86425ae40e739d6063d597e4816c29c
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-05-25 22:18:22 +0100

    basic modules

---
+++ Python/google.py
@@ -0,0 +1,4 @@
+person= {'name':'Roseline', 'Age' : 17, 'country': 'Nigeria'}
+print(person)
+def greeting(name):
+    print('welcome to Google' + name + '!')
---
+++ Python/welcome.py
@@ -0,0 +1,8 @@
+def message(name):
+    print("Welcome, " + name)
+
+person_2 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
